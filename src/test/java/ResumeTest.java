21f25af6eded00d3b0cdfa8bc06fd5d9f61aa13d
Evgeny <morozovei91@gmail.com>
2014-11-11 21:56:18 +0300

    Comment string //resume.name = "Test";

--- src/test/java/ResumeTest.java
+++ src/test/java/ResumeTest.java
@@ -11,7 +11,7 @@ public class ResumeTest {
     @Test
     public void testResume1() {
         Resume resume = new Resume();
-        resume.name = "Test";
+ //       resume.name = "Test";
 
     }
 
